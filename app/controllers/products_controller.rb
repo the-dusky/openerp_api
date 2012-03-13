8706e98562a9e9aa6263eaaa9d7cc3312eed7bd8
Sandy Corsillo <sandy@hickorees.com>
2012-03-13 17:55:59 -0400

    x

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,12 +2,12 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    # @products = params[:proudct]
-    # 
-    # respond_to do |format|
-    #   format.html # index.html.erb
-    #   format.json { render json: @products }
-    # end
+    @products = params[:name]
+       
+       respond_to do |format|
+         format.html # index.html.erb
+         format.json { render json: @products }
+       end
   end
 
   # GET /products/1
@@ -40,7 +40,24 @@ class ProductsController < ApplicationController
   # POST /products
   # POST /products.json
   def create
-    @products = params[:name]
+    @products = ProductProduct.create(
+      name:             params[:name],
+      cost_method:      params[:standard]
+              # 'description' => $value['product_description'],
+              #               'procure_method' => 'make_to_order',
+              #               'supply_method' => 'buy',
+              #               'sale_ok' => 'true', #Cand be Purchased
+              #               'purchase_ok' => 'true', #Can be Sold
+              #               'weight' => $value['product_weight'],
+              #               'variants' => $value['size_name'],
+              #               'company_id' => 3,
+              #               'default_code' => $value['accounting_id'], #ref
+              #               'list_price' => $value['product_price'], #sale_price
+              #               'standard_price' => $value['cost'], #cost
+              #               'property_account_income' => 197, #income_account
+              #               'property_account_expense' => 205, #cogs_account
+              #               'type' => 'product' #product_type ('product' = stockable_product)
+    )
 
     respond_to do |format|
       format.html # index.html.erb
