533f2464cfd54f61f829b44937ed714e389d8ee2
Sandy Corsillo <sandy@hickorees.com>
2012-03-14 17:11:36 -0400

    x

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -66,6 +66,26 @@ class ProductsController < ApplicationController
         product_id: params[:product_id],
         company_id: params[:company_id]
       )
+    else
+      @products = ProductProduct.create(
+        name: params[:name],
+        cost_method: 'standard',
+        description: params[:description],
+        procure_method: 'make_to_order',
+        supply_method: 'buy',
+        sale_ok: 'true', #Cand be Purchased
+        purchase_ok: 'true', #Can be Sold
+        weight: params[:weight],
+        variants: params[:variants],
+        company_id: 3,
+        default_code: params[:default_code], #ref
+        list_price: params[:list_price], #sale_price
+        standard_price: params[:standard_price], #cost
+        property_account_income: 197, #income_account
+        property_account_expense: 205, #cogs_account
+        type: 'product', #product_type ('product' = stockable_product)
+        product_tmpl_id: params[:product_tmpl_id]
+      )
     end
 
     respond_to do |format|
